ea51d54bae6435b01779188e1336a8d445f3f5c1
Aditya Kushwaha <kushwahaaaditya13@gmail.com>
2022-07-25 17:42:55 +0530

    Pilot Commit

---
+++ lua/user/lsp/init.lua
@@ -0,0 +1,23 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "user.lsp.configs"
+require("user.lsp.handlers").setup()
+require "user.lsp.null-ls"
+require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
