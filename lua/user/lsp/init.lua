da431178ebba84dfab236a35d7587e850e46e408
Aditya Kushwaha <kushwahaaaditya13@gmail.com>
2022-07-25 17:48:38 +0530

    repaired a warning

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,18 +6,4 @@ end
 require "user.lsp.configs"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
-require("transparent").setup({
-  enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
 
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
-  exclude = {}, -- table: groups you don't want to clear
-})
